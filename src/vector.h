1f50663a524010ba28e8f83bd9b00b8fa069f605
TUNAPRO1234 <60389660+tunapro1234@users.noreply.github.com>
2021-03-07 12:57:39 +0300

    indentation

--- src/vector.h
+++ src/vector.h
@@ -3,9 +3,6 @@
 
 #include "main.h"
 
-#define VEC_SLAVES 1
-#define VEC_DEBUG 1
-
 /*
  * vector_##TYPE##_t* vector_##TYPE##_init_h(size_t capacity);
  *	heapte vector oluşturur 
@@ -59,134 +56,128 @@
 	typedef struct vector_##TYPE##_t{	\
 		TYPE *start, *end, *capacity;	\
 	} vector_##TYPE##_t;				\
-	 \
-	vector_##TYPE##_t* vector_##TYPE##_init_h(size_t capacity); \
-	vector_##TYPE##_t vector_##TYPE##_init(size_t capacity); \
-	 \
-	void* vector_##TYPE##_destroy(vector_##TYPE##_t *self); \
- \
-	size_t vector_##TYPE##_capacity(const vector_##TYPE##_t *self); \
-	size_t vector_##TYPE##_length(const vector_##TYPE##_t *self); \
-	size_t vector_##TYPE##_size(const vector_##TYPE##_t *self); \
-	 \
-	vector_##TYPE##_t* vector_##TYPE##_move_h(vector_##TYPE##_t *self); \
-	vector_##TYPE##_t vector_##TYPE##_move(vector_##TYPE##_t *self); \
-	vector_##TYPE##_t* vector_##TYPE##_copy_h(const vector_##TYPE##_t *self); \
-	vector_##TYPE##_t vector_##TYPE##_copy(const vector_##TYPE##_t *self); \
- \
-	bool vector_##TYPE##_extend_capacity(vector_##TYPE##_t *self); \
-	bool vector_##TYPE##_push_back(vector_##TYPE##_t *self, TYPE data); \
-	bool vector_##TYPE##_p_push_back(vector_##TYPE##_t *self, TYPE *data); \
-	 \
-	TYPE *vector_##TYPE##_get(vector_##TYPE##_t *self, size_t index); \
-	TYPE vector_##TYPE##_read(const vector_##TYPE##_t *self, size_t index); \
- \
-	/********************************************************************************/ \
- \
-	vector_##TYPE##_t* vector_##TYPE##_init_h(size_t capacity) { \
-		vector_##TYPE##_t *new_vector = (vector_##TYPE##_t *)malloc(sizeof(vector_##TYPE##_t));  \
-		 \
-		new_vector->start 		= (TYPE *)malloc(sizeof(TYPE) * capacity); \
-		new_vector->capacity	= new_vector->start + capacity; \
-		new_vector->end			= new_vector->start; \
-		return new_vector; \
-	} \
-	 \
-	vector_##TYPE##_t vector_##TYPE##_init(size_t capacity) { \
-		vector_##TYPE##_t new_vector; \
-		 \
-		new_vector.start 		= (TYPE *)malloc(sizeof(TYPE) * capacity); \
-		new_vector.capacity		= new_vector.start + capacity; \
-		new_vector.end			= new_vector.start; \
-		return new_vector; \
-	} \
- \
-	void* vector_##TYPE##_destroy(vector_##TYPE##_t *self) {  \
-		free(self->start); \
-	} \
- \
-	size_t vector_##TYPE##_capacity(const vector_##TYPE##_t *self) { return self->capacity - self->start; } \
-	size_t vector_##TYPE##_length(const vector_##TYPE##_t *self) { return self->end - self->start; } \
-	size_t vector_##TYPE##_size(const vector_##TYPE##_t *self) { return self->end - self->start; } \
-	 \
-	vector_##TYPE##_t* vector_##TYPE##_move_h(vector_##TYPE##_t *self) { \
-		vector_##TYPE##_t *new_vector = (vector_##TYPE##_t *)malloc(sizeof(vector_##TYPE##_t)); \
- \
-		new_vector->capacity = self->capacity; \
-		new_vector->start = self->start; \
-		new_vector->end = self->end; \
- \
-		self->capacity = NULL; \
-		self->start = NULL; \
-		self->end = NULL; \
- \
-		return new_vector; \
-	} \
- \
-	vector_##TYPE##_t vector_##TYPE##_move(vector_##TYPE##_t *self) { \
-		vector_##TYPE##_t new_vector; \
- \
-		new_vector.capacity = self->capacity; \
-		new_vector.start = self->start; \
-		new_vector.end = self->end; \
- \
-		self->capacity = NULL; \
-		self->start = NULL; \
-		self->end = NULL; \
- \
-		return new_vector; \
-	} \
- \
-	vector_##TYPE##_t* vector_##TYPE##_copy_h(const vector_##TYPE##_t *self) { \
-		size_t length = vector_##TYPE##_length(self); \
-		vector_##TYPE##_t *new_vector = vector_##TYPE##_init_h(vector_##TYPE##_capacity(self)); \
-		memcpy(new_vector->start, self->start, vector_##TYPE##_capacity(self) * sizeof(TYPE)); \
-		new_vector->end = new_vector->start + length; \
-		return new_vector; \
-	} \
-	 \
-	vector_##TYPE##_t vector_##TYPE##_copy(const vector_##TYPE##_t *self) { \
-		size_t length = vector_##TYPE##_length(self); \
-		vector_##TYPE##_t new_vector = vector_##TYPE##_init(vector_##TYPE##_capacity(self)); \
-		memcpy(new_vector.start, self->start, vector_##TYPE##_capacity(self) * sizeof(TYPE)); \
-		new_vector.end = new_vector.start + length; \
-		return new_vector; \
-	} \
- \
-	bool vector_##TYPE##_extend_capacity(vector_##TYPE##_t *self) { \
-		size_t new_capacity = vector_##TYPE##_capacity(self) * 2; \
-		size_t length 		= vector_##TYPE##_length(self); \
- \
-		void *tmp = realloc(self->start, new_capacity * sizeof(TYPE)); \
-		if (tmp == NULL) return False; \
- \
-		self->start 	= (TYPE *)tmp; \
-		self->capacity 	= self->start + new_capacity; \
-		self->end 		= self->start + length; \
-		return True; \
-	} \
- \
-	bool vector_##TYPE##_push_back(vector_##TYPE##_t *self, TYPE data) { \
-		if (self->end == self->capacity){ \
-			if (vector_##TYPE##_extend_capacity(self) == False) \
-				return False; \
-		} \
-		*self->end = data; \
-		self->end++; \
-		return True; \
-	} \
- \
-	bool vector_##TYPE##_p_push_back(vector_##TYPE##_t *self, TYPE *data) { \
-		if (self->end == self->capacity){ \
-			if (vector_##TYPE##_extend_capacity(self) == False) \
-				return False; \
-		} \
-		memcpy(self->end, data, sizeof(TYPE)); \
-		self->end++; \
-		return True; \
-	} \
 \
-	TYPE *vector_##TYPE##_get(vector_##TYPE##_t *self, size_t index) { return self->start + index; } \
-	TYPE vector_##TYPE##_read(const vector_##TYPE##_t *self, size_t index) { return *(self->start + index); }
+	vector_##TYPE##_t* vector_##TYPE##_init_h(size_t capacity); \
+	vector_##TYPE##_t vector_##TYPE##_init(size_t capacity); 	\
+	void* vector_##TYPE##_destroy(vector_##TYPE##_t *self); 	\
+\
+	size_t vector_##TYPE##_capacity(const vector_##TYPE##_t *self); \
+	size_t vector_##TYPE##_length(const vector_##TYPE##_t *self); 	\
+	size_t vector_##TYPE##_size(const vector_##TYPE##_t *self); 	\
+\
+	vector_##TYPE##_t* vector_##TYPE##_move_h(vector_##TYPE##_t *self); 		\
+	vector_##TYPE##_t vector_##TYPE##_move(vector_##TYPE##_t *self); 			\
+	vector_##TYPE##_t* vector_##TYPE##_copy_h(const vector_##TYPE##_t *self);	\
+	vector_##TYPE##_t vector_##TYPE##_copy(const vector_##TYPE##_t *self); 		\
+ \
+	bool vector_##TYPE##_extend_capacity(vector_##TYPE##_t *self); 			\
+	bool vector_##TYPE##_push_back(vector_##TYPE##_t *self, TYPE data); 	\
+	bool vector_##TYPE##_p_push_back(vector_##TYPE##_t *self, TYPE *data); 	\
+	 \
+	TYPE *vector_##TYPE##_get(vector_##TYPE##_t *self, size_t index); 		\
+	TYPE vector_##TYPE##_read(const vector_##TYPE##_t *self, size_t index); \
+\
+/********************************************************************************/ \
+\
+	vector_##TYPE##_t* vector_##TYPE##_init_h(size_t capacity) { 									\
+		vector_##TYPE##_t *new_vector = (vector_##TYPE##_t *)malloc(sizeof(vector_##TYPE##_t));  	\
+		\
+		new_vector->start 		= (TYPE *)malloc(sizeof(TYPE) * capacity); 							\
+		new_vector->capacity	= new_vector->start + capacity; 									\
+		new_vector->end			= new_vector->start; 												\
+		return new_vector; 																			\
+	}	\
+\
+	vector_##TYPE##_t vector_##TYPE##_init(size_t capacity) { \
+		vector_##TYPE##_t new_vector; 										\
+		\
+		new_vector.start 		= (TYPE *)malloc(sizeof(TYPE) * capacity); 	\
+		new_vector.capacity		= new_vector.start + capacity; 				\
+		new_vector.end			= new_vector.start; 						\
+		return new_vector; 													\
+	} 	\
+\
+	void* vector_##TYPE##_destroy(vector_##TYPE##_t *self) {  	\
+		free(self->start); 										\
+	} 	\
+\
+	size_t vector_##TYPE##_capacity(const vector_##TYPE##_t *self) { return self->capacity - self->start; } \
+	size_t vector_##TYPE##_length(const vector_##TYPE##_t *self) { return self->end - self->start; } 		\
+	size_t vector_##TYPE##_size(const vector_##TYPE##_t *self) { return self->end - self->start; } 			\
+\
+	vector_##TYPE##_t* vector_##TYPE##_move_h(vector_##TYPE##_t *self) { 						\
+		vector_##TYPE##_t *new_vector = (vector_##TYPE##_t *)malloc(sizeof(vector_##TYPE##_t)); \
+		new_vector->capacity = self->capacity; 													\
+		new_vector->start = self->start; 														\
+		new_vector->end = self->end; 															\
+		self->capacity = NULL; 																	\
+		self->start = NULL; 																	\
+		self->end = NULL; 																		\
+		return new_vector; 																		\
+	} 	\
+\
+	vector_##TYPE##_t vector_##TYPE##_move(vector_##TYPE##_t *self) { \
+		vector_##TYPE##_t new_vector; 			\
+		new_vector.capacity = self->capacity; 	\
+		new_vector.start = self->start; 		\
+		new_vector.end = self->end; 			\
+		self->capacity = NULL; 					\
+		self->start = NULL; 					\
+		self->end = NULL; 						\
+		return new_vector; 						\
+	} 	\
+\
+	vector_##TYPE##_t* vector_##TYPE##_copy_h(const vector_##TYPE##_t *self) { 					\
+		size_t length = vector_##TYPE##_length(self); 											\
+		vector_##TYPE##_t *new_vector = vector_##TYPE##_init_h(vector_##TYPE##_capacity(self)); \
+		memcpy(new_vector->start, self->start, vector_##TYPE##_capacity(self) * sizeof(TYPE)); 	\
+		new_vector->end = new_vector->start + length; 											\
+		return new_vector; 																		\
+	} 	\
+\
+	vector_##TYPE##_t vector_##TYPE##_copy(const vector_##TYPE##_t *self) { 					\
+		size_t length = vector_##TYPE##_length(self); 											\
+		vector_##TYPE##_t new_vector = vector_##TYPE##_init(vector_##TYPE##_capacity(self)); 	\
+		memcpy(new_vector.start, self->start, vector_##TYPE##_capacity(self) * sizeof(TYPE)); 	\
+		new_vector.end = new_vector.start + length; 											\
+		return new_vector; 																		\
+	} 	\
+\
+	bool vector_##TYPE##_extend_capacity(vector_##TYPE##_t *self) { 	\
+		size_t new_capacity = vector_##TYPE##_capacity(self) * 2; 		\
+		size_t length 		= vector_##TYPE##_length(self); 			\
+		\
+		void *tmp = realloc(self->start, new_capacity * sizeof(TYPE)); 	\
+		if (tmp == NULL) return False; 									\
+		\
+		self->start 	= (TYPE *)tmp; 					\
+		self->capacity 	= self->start + new_capacity; 	\
+		self->end 		= self->start + length; 		\
+		return True; 									\
+	} 	\
+\
+	bool vector_##TYPE##_push_back(vector_##TYPE##_t *self, TYPE data) { 	\
+		if (self->end == self->capacity){ 									\
+			if (vector_##TYPE##_extend_capacity(self) == False) 			\
+				return False; 												\
+		} 																	\
+		*self->end = data; 													\
+		self->end++; 														\
+		return True; 														\
+	} 	\
+\
+	bool vector_##TYPE##_p_push_back(vector_##TYPE##_t *self, TYPE *data) { \
+		if (self->end == self->capacity){ 									\
+			if (vector_##TYPE##_extend_capacity(self) == False) 			\
+				return False; 												\
+		} 																	\
+		memcpy(self->end, data, sizeof(TYPE)); 								\
+		self->end++; 														\
+		return True; 														\
+	} 	\
+\
+	TYPE *vector_##TYPE##_get(vector_##TYPE##_t *self, size_t index) { return self->start + index; } 			\
+	TYPE vector_##TYPE##_read(const vector_##TYPE##_t *self, size_t index) { return *(self->start + index); } 	\
+
 
 #endif
